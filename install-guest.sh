c6c68d19643c767537fbd14b537526a643504517
Manuel Kirstetter <manu@rocketlab.be>
2014-04-01 23:12:43 +0200

    Create install-guest.sh

---
+++ install-guest.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+curl --insecure https://www.npmjs.org/install.sh | sudo bash
+
+sudo npm install -g grunt-cli
+
+cd /vagrant
+npm install grunt --save-dev 
+npm install grunt-contrib-concat --save-dev 
+npm install grunt-contrib-less --save-dev 
+npm install grunt-contrib-uglify --save-dev 
+npm install grunt-contrib-watch --save-dev 
+npm install grunt-phpunit --save-dev
+
+sudo npm install -g bower
+
+bower install bootstrap -S
